a16e00475cca6c9dd89a0647420659eb4a5b7e9d
juantonova <j.konina@mail.ru>
2022-09-28 14:14:40 +0300

    Added Card

--- views/CardList.jsx
+++ views/CardList.jsx
@@ -2,11 +2,20 @@ const React = require('react');
 const Layout = require('./Layout');
 
 function CardsList() {
-    return(<Layout>
-        <div>
-            <p>It's cardlist</p>
-        </div>
-    </Layout>)
+  return (
+    <Layout>
+      <div className="card-list container-xxl"> 
+      <div className='card'>
+      <div class="card-body">
+      <img src="..." class="card-img-top" alt="..."   />
+    <h5 class="card-title">Название лекарства</h5>
+    <p class="card-text">Price</p>
+    <a href="#" class="btn btn-primary">Buy</a>
+  </div> 
+      </div>
+      </div>
+    </Layout>
+  );
 }
 
 module.exports = CardsList;
--- views/Layout.jsx
+++ views/Layout.jsx
@@ -2,21 +2,22 @@ const React = require('react');
 
 function Layout({ children }) {
   return (
-  <html lang="ru">
-    <head>
-      <meta charset="UTF-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="stylesheet" href="/css/bootstrap.min.css" />
-      <link rel="stylesheet" href="/css/main.css" />
-      <script defer src="/js/bootstrap.bundle.min.js" />
-      <script defer src="/js/main.js" />
-      <title>Farmacy</title>
-    </head>
-    <body>
-      {children}
-    </body>
-  </html>)
+    <html lang="ru">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/css/main.css" />
+        <script defer src="/js/bootstrap.bundle.min.js" />
+        <script defer src="/js/main.js" />
+        <title>Farmacy</title>
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
 }
 
 module.exports = Layout;
