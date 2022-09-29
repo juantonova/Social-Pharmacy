b14c53d76aff5f87389250baaa24bbe331f7546d
Angelika <surovtseva.ang@mail.ru>
2022-09-29 12:24:32 +0300

    render checked(final version)

--- middleware/getUser.js
+++ middleware/getUser.js
@@ -1,10 +1,10 @@
 const { User } = require('../db/models');
 
 async function getUser(req, res, next) {
-  if (req.session.user_id) {
+  if (req.session?.user_id) {
     const user = await User.findByPk((req.session.user_id));
     res.locals.user = user;
-    // console.log(user);
+    console.log(user);
   }
   next();
 }
