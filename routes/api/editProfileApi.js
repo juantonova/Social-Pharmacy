7a415fe9d475d92b1674d77a3e088c582bf0e908
Rushaniya <rusha359@gmail.com>
2022-09-29 13:20:13 +0300

    delete

--- routes/api/editProfileApi.js
+++ routes/api/editProfileApi.js
@@ -1,26 +1,24 @@
-const router = require("express").Router();
-const { User } = require("../../db/models");
+const router = require('express').Router();
+const { User } = require('../../db/models');
 
 // роутер на изменение профиля
-router.put("/:id", async (req, res) => {
-  const { name, email } = req.body;
-  const user = await User.findOne({ where: { id: req.params.id } });
-  user.name = name;
-  user.email = email;
-  user.save();
-  res.json({ updated: true });
-});
-
-// роутер на удаление профиля
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  User.destroy({ where: { id } })
-    .then((deletedUser) =>
-      deletedUser
-        ? res.json({ deleted: true, id })
-        : res.status(404).json({ deleted: false })
-    )
-    .catch((error) => res.status(500).json({ message: error.message }));
-});
+router
+  .route('/profile/:id')
+  .put(async (req, res) => {
+    const { name, email } = req.body;
+    const user = await User.findOne({ where: { id: req.params.id } });
+    user.name = name;
+    user.email = email;
+    user.save();
+    res.json({ updated: true });
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    User.destroy({ where: { id } })
+      .then((deletedUser) =>
+        deletedUser ? res.json({ deleted: true, id }) : res.status(404).json({ deleted: false })
+      )
+      .catch((error) => res.status(500).json({ message: error.message }));
+  });
 
 module.exports = router;
--- routes/render/RegistrationRouter.js
+++ routes/render/RegistrationRouter.js
@@ -4,32 +4,34 @@ const { User } = require('../../db/models');
 const Registration = require('../../views/Registration.jsx');
 
 router.get('/', (req, res) => {
-    res.renderComponent(Registration, { });
+  res.renderComponent(Registration, {});
 });
 
 router.post('/', async (req, res) => {
-    const { name, email, password, confirmPassword } = req.body;
-    try {
-        const user = await User.findOne({ where: { email }, raw: true,});
+  const { name, email, password, confirmPassword } = req.body;
+  try {
+    const user = await User.findOne({ where: { email }, raw: true });
 
-        if (user) {
-            res.json({ status: 'ok', message: 'Пользователь с таким именем уже существует'});
-        }
-        if (password.length < 8) {
-            res.json({status: 'ok', message: 'Длина пароля должна быть не меньше 8 символов!'});
-            return;
-        }
-        if (password === confirmPassword) {
-            const hashedPassword = await bcrypt.hash(password, 10);
-            const newUser = await User.create({
-                name, email, password: hashedPassword,
-            });
-        }
-        req.session.user = { id: newUser.id };
-        res.status(200).json({text: 'ok '});
-    } catch (err) {
-        res.json({message: `${err.message}`});
+    if (user) {
+      res.json({ status: 'ok', message: 'Пользователь с таким именем уже существует' });
     }
+    if (password.length < 8) {
+      res.json({ status: 'ok', message: 'Длина пароля должна быть не меньше 8 символов!' });
+      return;
+    }
+    if (password === confirmPassword) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+      });
+    }
+    req.session.user = { id: newUser.id };
+    res.status(200).json({ text: 'ok ' });
+  } catch (err) {
+    res.json({ message: `${err.message}` });
+  }
 });
 
 module.exports = router;
