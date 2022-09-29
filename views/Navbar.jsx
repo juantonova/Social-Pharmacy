634a92a7caa58b3b67f03f1b34ed2fc6d4889280
Rushaniya <rusha359@gmail.com>
2022-09-29 15:22:54 +0300

    navbar- href profile_id

--- views/Navbar.jsx
+++ views/Navbar.jsx
@@ -3,18 +3,34 @@ const React = require('react');
 module.exports = function Navbar({ user }) {
   return (
     <div>
-      { user ? (
+      {user ? (
         <nav className="navbar navbar-expand-lg bg-light">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <a className="navbar-brand" href="#">
+              Navbar
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
               <span className="navbar-toggler-icon" />
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div className="navbar-nav">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-                <a className="nav-link" href="/login/logout">Logout</a>
-                <a className="nav-link" href="#">Personal Cabinet</a>
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+                <a className="nav-link" href="/login/logout">
+                  Logout
+                </a>
+                <a className="nav-link" href={`/profile/${user.id}`}>
+                  Personal Cabinet
+                </a>
               </div>
             </div>
           </div>
@@ -22,15 +38,31 @@ module.exports = function Navbar({ user }) {
       ) : (
         <nav className="navbar navbar-expand-lg bg-light">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <a className="navbar-brand" href="#">
+              Navbar
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
               <span className="navbar-toggler-icon" />
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div className="navbar-nav">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-                <a className="nav-link" href="/login">Login</a>
-                <a className="nav-link" href="/registration">Registration</a>
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+                <a className="nav-link" href="/login">
+                  Login
+                </a>
+                <a className="nav-link" href="/registration">
+                  Registration
+                </a>
               </div>
             </div>
           </div>
